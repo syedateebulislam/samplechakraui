31e5ac140c4e22b850361f23d9a563b32d868c5b
ateeb <syedateebulislam@gmail.com>
2021-07-25 15:40:44 +0530

    updated css

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -7,17 +7,17 @@ function Social() {
             <HStack spacing="20">
 
                 <Button>
-                <Icon as={FaMediumM} boxSize="10" 
+                <Icon as={FaMediumM} boxSize="7" 
                 onClick={() => window.open("https://syedateebulislam.medium.com")}/>
                 </Button>    
 
                 <Button>
-                <Icon as={FaFacebookF} boxSize="10"
+                <Icon as={FaFacebookF} boxSize="7"
                 onClick={() => window.open("https://m.facebook.com/people/Syed-Ateebul-Islam/100007216245323/")}/>
                 </Button>
                 
                 <Button>
-                <Icon as={FaGoogle} boxSize="10"
+                <Icon as={FaGoogle} boxSize="7"
                 onClick={() => window.open("https://www.google.co.in/search?q=syed+ateebul+islam&source=hp&ei=uVr0YILXIbG-3LUPubq70AY&iflsig=AINFCbYAAAAAYPRoyQThTnTHrEV5GWHh8WRFGKPkgEhb&oq=syed+ateebul+islam&gs_lcp=Cgdnd3Mtd2l6EAMyBwghEAoQoAE6CggAEOoCELQCEEM6DQgAEOoCELQCEEMQiwM6CAgAELEDEIMBOggILhCxAxCDAToFCAAQsQM6AggAOgsILhCxAxDHARCjAjoFCC4QsQM6CAguEMcBEK8BOgUILhCTAjoCCC46CAguELEDEJMCOgYIABAWEB46CAgAEBYQChAeOgYIABANEB46CAgAEAgQDRAeOggIABANEAoQHlCod1jwlQFgj5gBaAFwAHgAgAHAAogBuBaSAQgwLjE2LjEuMZgBAKABAaoBB2d3cy13aXqwAQq4AQI&sclient=gws-wiz&ved=0ahUKEwjCl-fqiO3xAhUxH7cAHTndDmoQ4dUDCAg&uact=5")}/>
                 </Button>
 
